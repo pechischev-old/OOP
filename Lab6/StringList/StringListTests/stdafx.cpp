b53400fa86359afd1f258165c583cd3d743623d1
Vladimir Pechishev <pechishev.volodya@yandex.ru>
2016-05-18 19:49:41 +0300

    добавил лабу по CStringList

---
+++ Lab6/StringList/StringListTests/stdafx.cpp
@@ -0,0 +1,9 @@
+// stdafx.cpp: исходный файл, содержащий только стандартные включаемые модули
+// StringListTests.pch будет предкомпилированным заголовком
+// stdafx.obj будет содержать предварительно откомпилированные сведения о типе
+
+#include "stdafx.h"
+
+#define BOOST_TEST_NO_MAIN 
+// подключаем реализацию boost test
+#include <boost/test/included/unit_test.hpp>
